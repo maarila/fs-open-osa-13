179c00068801c65d588aafe3fa75c211854413c2
Mika Äärilä <maarila@gmail.com>
2024-05-06 14:11:55 +0300

    Return exercise 13.4

--- index.js
+++ index.js
@@ -1,23 +1,65 @@
 require('dotenv').config()
-const { Sequelize, QueryTypes } = require('sequelize')
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, { logging: false })
+const app = express()
+app.use(express.json())
 
-const main = async () => {
-  try {
-    await sequelize.authenticate()
-    const blogs = await sequelize.query('SELECT * FROM blogs', {
-      type: QueryTypes.SELECT,
-    })
-    blogs.forEach((blog) =>
-      console.log(
-        blog.author + ": '" + blog.title + "', " + blog.likes + ' likes'
-      )
-    )
-    sequelize.close()
-  } catch (error) {
-    console.error('Unable to connect to the database:', error)
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+class Blog extends Model {}
+
+Blog.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    author: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    likes: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: false,
+    modelName: 'blog',
   }
-}
+)
 
-main()
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id)
+  blog.destroy()
+  return res.status(200).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
