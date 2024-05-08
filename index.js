840e6c0d054e1fdfebb45e82aa9a7dcfdfa4ca95
Mika Äärilä <maarila@gmail.com>
2024-05-08 20:27:05 +0300

    Return exercise 13.8

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const express = require('express')
 const app = express()
 
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 
 app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 const errorHandler = (error, request, response, next) => {
   if (error.name === 'SequelizeDatabaseError') {
