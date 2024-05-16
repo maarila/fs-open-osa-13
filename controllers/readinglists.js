6cdfb75e0f47be1b7dc23ff8207b95557d4b16b3
Mika Äärilä <maarila@gmail.com>
2024-05-16 07:27:19 +0300

    Return exercise 13.22

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,6 +1,27 @@
 const router = require('express').Router()
 const { Readinglist } = require('../models')
-const sequelize = require('sequelize')
+const jwt = require('jsonwebtoken')
+const { SECRET } = require('../util/config')
+
+const tokenExtractor = (req, res, next) => {
+  const authorization = req.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    try {
+      req.decodedToken = jwt.verify(authorization.substring(7), SECRET)
+    } catch (error) {
+      console.log(error)
+      return res.status(401).json({ error: 'token invalid' })
+    }
+  } else {
+    return res.status(401).json({ error: 'token missing' })
+  }
+  next()
+}
+
+const readinglistFinder = async (req, res, next) => {
+  req.readinglist = await Readinglist.findByPk(req.params.id)
+  next()
+}
 
 router.post('/', async (req, res) => {
   try {
@@ -15,5 +36,22 @@ router.post('/', async (req, res) => {
     return res.status(400).json({ error: 'invalid data' })
   }
 })
+
+router.put(
+  '/:id',
+  readinglistFinder,
+  tokenExtractor,
+  async (req, res, next) => {
+    if (req.readinglist && req.body.read) {
+      if (req.readinglist.userId === req.decodedToken.id) {
+        req.readinglist.read = req.body.read
+        await req.readinglist.save()
+        res.json(req.readinglist)
+      }
+    } else {
+      res.status(404).end()
+    }
+  }
+)
 
 module.exports = router
